90e7a2c948090123110229b4aa52f5448c70dab4
NekoYo1 <kostyafear203@gmail.com>
2022-11-12 12:35:32 +1000

    вроде сделал

--- WPF_FILIPPOV_PR11/Window1.xaml.cs
+++ WPF_FILIPPOV_PR11/Window1.xaml.cs
@@ -10,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPF_FILIPPOV_PR11
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window1 window1 = new Window1();
-            window1.Show();
+            Window2 window2 = new Window2();
+            window2.Show();
         }
     }
 }
